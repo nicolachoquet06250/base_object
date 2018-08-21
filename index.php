2cb3f1cdc465216df1f2e0de7df5c0f8fb8ee60e
nicolas.choquet <nicolas.choquet@lagardere-active.com>
2018-08-21 11:36:11 +0200

    liaison de toutes les parties du framework pour que tout fonctionne en bon termes

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
 require_once 'autoload.php';
 
 Project::main(function () {
-
-	var_dump(dao_manager::create()->get_dao_from('user', 'prenom', 'Yann')->get_field('prenom'));
-	var_dump(dao_manager::create()->get_dao_from('slider', 'name', 'Slider 1')->get_field('src'));
-	var_dump(dao_manager::create()->get_dao_list('user')->get(0)->get_field('nom'));
-
+	var_dump(
+		dao_manager::create()->get_dao_from('user', 'prenom', 'Yann')->get_field('prenom'),
+		dao_manager::create()->get_dao_from('slider', 'name', 'Slider 1')->get_field('src'),
+		dao_manager::create()->get_dao_list('user')->get(0)->get_field('nom')
+	);
 });
