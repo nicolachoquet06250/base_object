ef49826500e9aa9a1b71104266564f407f658624
nicolas.choquet <nicolas.choquet@lagardere-active.com>
2018-08-27 11:14:27 +0200

    mise en place d'un système de pages avec le nom de la méthode statique de démarage du projet.

--- index.php
+++ index.php
@@ -10,47 +10,45 @@ use project\services\managers\dao_manager;
 use project\sql\json;
 use project\utils\Project;
 
-//ini_set('display_errors', 'on');
-
 require_once 'autoload.php';
 
-Project::main(function ($_this) {
+Project::Accueil(function ($_this, $args) {
+	$page_name = $args['page_name'];
 	/**
 	 * @var Project $_this
 	 * @var user_dao $user
 	 */
 
-//	$prenom = dao_manager::create()->get_dao_from('user', 'prenom', 'Yann')->get_field('prenom');
-//	$src = dao_manager::create()->get_dao_from('slider', 'name', 'Slider 1')->get_field('src');
-//
-//	$user = dao_manager::create()->get_dao_list('user')->get(0);
-//	$nom = $user->get_field('nom');
-//
-//	$new_user = dao_manager::create()
-//						   ->create_dao('user')
-//						   ->create_new(
-//						   	0, 'Choquet', 'Nicolas',
-//							'1102 ch de l\'espagnol', 'Les primevères',
-//							'06250', 'Mougins', '1995-07-21',
-//							date('Y-m-d'));
-//	$test_afficher_body = $_this->get_object('test_afficher_body');
-//
-//	echo "--------------------------- dao ---------------------------\n";
-//	var_dump($prenom);
-//	var_dump($src);
-//	var_dump($nom);
-//	var_dump($new_user->get_field('date_naissence'));
-//
-//	echo "--------------------------- util --------------------------\n";
-//	var_dump('test avec la méthode => '
-//			 .$_this->get_method(test_afficher_body::class, 'display'));
-//	var_dump('test sans la méthode => '
-//			 .$test_afficher_body->display());
-//	var_dump('test statique avec la méthode => '
-//			 .$_this->get_static_method(test_afficher_body::class, 'toto', 'test', 'avec', 'des', 'tapettes'));
-//	var_dump('test statique sans la méthode => '
-//			 .test_afficher_body::toto('test', 'avec', 'des', 'tapettes'));
-//	var_dump($_this->is_object($test_afficher_body));
+	$_this->var_dump($page_name);
+
+	$prenom = dao_manager::create()->get_dao_from('user', 'prenom', 'Yann')->get_field('prenom');
+	$src = dao_manager::create()->get_dao_from('slider', 'name', 'Slider 1')->get_field('src');
+
+	$user = dao_manager::create()->get_dao_list('user')->get(0);
+	$nom = $user->get_field('nom');
+
+	$new_user = dao_manager::create()
+						   ->create_dao('user')
+						   ->create_new(
+						   	0, 'Choquet', 'Nicolas',
+							'1102 ch de l\'espagnol', 'Les primevères',
+							'06250', 'Mougins', '1995-07-21',
+							date('Y-m-d'));
+	$test_afficher_body = $_this->get_object('test_afficher_body');
+
+	echo "--------------------------- dao ---------------------------\n";
+	$_this->var_dump($prenom, $src, $nom, $new_user->get_field('date_naissence'));
+
+	echo "--------------------------- util --------------------------\n";
+	$_this->var_dump('test avec la méthode => '
+			 .$_this->get_method(test_afficher_body::class, 'display'),
+					 'test sans la méthode => '
+			 .$test_afficher_body->display(),
+					 'test statique avec la méthode => '
+			 .$_this->get_static_method(test_afficher_body::class, 'toto', 'test', 'avec', 'des', 'tapettes'),
+					 'test statique sans la méthode => '
+					 .test_afficher_body::toto('test', 'avec', 'des', 'tapettes'),
+			 $_this->is_object($test_afficher_body));
 
 	$sql_connector = $_this->sql_connector('json', 'account');
 
--- utils/Project.php
+++ utils/Project.php
@@ -11,18 +11,37 @@ class Project extends util {
 	 * @param callable $callback
 	 * @param callable|null $catch
 	 * @param array $args
+	 * @param string $page_name
 	 */
-	public static function main(callable $callback, callable $catch = null, $args = []) {
+	public static function main(callable $callback, callable $catch = null, $args = [], $page_name = 'main') {
+		$args['page_name'] = $page_name;
 		try {
 			$callback(new Project(), $args);
 		}
 		catch (Exception $e) {
 			if($catch) {
-				$catch($e, new Project());
+				$catch($e, new Project(), $args);
 			}
 			else {
 				echo $e->getMessage()."\n";
 			}
 		}
+	}
+
+	public static function __callStatic($name, $arguments) {
+		$callback = $arguments[0];
+		$catch = null;
+		$args = [];
+		if(isset($arguments[1]) && gettype($arguments[1]) === 'array') {
+			$args = $arguments[1];
+		}
+		elseif (isset($arguments[1]) && gettype($arguments[1]) === 'object' && get_class($arguments[1]) === 'Closure') {
+			$catch = $arguments[1];
+			if(isset($arguments[2])) {
+				$args = $arguments[2];
+			}
+		}
+		$page_name = $name;
+		self::main($callback, $catch, $args, $page_name);
 	}
 }
